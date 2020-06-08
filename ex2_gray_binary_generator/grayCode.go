40de58ad4f74657c135e6ae79429c9e26e0bee94
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-06-08 11:46:38 +0900

    Fix calculating part

--- ex2_gray_binary_generator/grayCode.go
+++ ex2_gray_binary_generator/grayCode.go
@@ -3,40 +3,44 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func convertToGray(num int) int {
-	return num ^ (num >> 1)
+func printCode(arr []int, len int, ch chan []int) {
+
+	snd := make([]int, len)
+	for i := 0; i < len; i++ {
+		snd[i] = arr[i]
+	}
+	ch <- snd
+}
+
+func printGray(arr []int, n, index, reverse int, ch chan []int) {
+	if index == n {
+		printCode(arr, n, ch)
+		return
+	}
+
+	arr[index] = reverse
+	printGray(arr, n, index+1, 0, ch)
+	arr[index] = 1 - reverse
+	printGray(arr, n, index+1, 1, ch)
 }
 
 func GrayBinaryGenerator(n int) <-chan []int {
 	GrayCodeStream := make(chan []int)
 
-	rng := 1
-
-	for i := 0; i < n; i++ {
-		rng *= 2
-	}
 	go func() {
 		defer close(GrayCodeStream)
-		var x string
+
 		arr := make([]int, n)
+		printGray(arr, n, 0, 0, GrayCodeStream)
 
-		for i := 0; i < rng; i++ {
-			x = fmt.Sprintf("%0*b", n, convertToGray(i))
-
-			for j := 0; j < n; j++ {
-				arr[j], _ = strconv.Atoi(string(x[j]))
-			}
-			GrayCodeStream <- arr
-		}
 	}()
 	return GrayCodeStream
 }
 
 func main() {
-	for i := range GrayBinaryGenerator(5) {
+	for i := range GrayBinaryGenerator(3) {
 		fmt.Println(i)
 	}
 }
